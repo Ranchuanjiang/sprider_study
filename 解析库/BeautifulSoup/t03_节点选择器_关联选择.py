e8b6ff2b944fe28166d400a6058a47c3d94849b4
Ranchuanjiang <754596974@qq.com>
2018-09-14 14:41:49 +0800

    study

--- 解析库/BeautifulSoup/t03_节点选择器_关联选择.py
+++ 解析库/BeautifulSoup/t03_节点选择器_关联选择.py
@@ -82,4 +82,5 @@ with open("taobao2.html", "rb") as f:
     # 父节点 和 祖先节点 用parents 属性
     print(soup.p.a)
     print(list(enumerate(soup.p.a.parents)))
-    # 兄弟节点选择 用next_sibling 和 previ_sibling 分别获取下一个和上一个兄弟元素 next_siblings 和 previ_sibings 获取前面的 和后面的兄弟元素
+    # 兄弟节点选择 用next_sibling 和 previous_sibling 分别获取下一个和上一个兄弟元素 next_siblings 和 previous_sibings 获取前面的 和后面的兄弟元素
+
