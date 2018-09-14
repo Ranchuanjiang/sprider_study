f907983043c8a6b53ac0594fde884dc5e9748227
Ranchuanjiang <754596974@qq.com>
2018-09-14 14:40:58 +0800

    study

--- 解析库/BeautifulSoup/t03_节点选择器_关联选择.py
+++ 解析库/BeautifulSoup/t03_节点选择器_关联选择.py
@@ -14,8 +14,72 @@ with open("taobao2.html", "rb") as f:
 </span>
          会员俱乐部
         </a>, '\n']
-    可以看到 所列出来的节点 包含了直接子节点 里面的节点 但是是一块的 想要 在获得子节点里面的子节点 需要用到children
+    也可以用children 来查看不过是一个生成器类型
     """
     print(soup.p.children)
-    for child in soup.p.children:
-        print(child)
+    for i, child in enumerate(soup.p.children):
+        print(i, "---->", child)
+
+    """
+    输出:
+        0 ----> 
+    
+    1 ----> <a class="J_MemberPunch h" data-spm="d3" href="//taojinbi.taobao.com/index.htm?auto_take=true">
+    <span class="tbh-icon">
+    </span>
+             领淘金币抵钱
+            </a>
+    2 ----> 
+    
+    3 ----> <a class="J_MemberClub h club" href="//vip.taobao.com">
+    <span class="tbh-icon">
+    </span>
+             会员俱乐部
+            </a>
+    4 ----> 
+    
+    """
+    # 想要查看所有的子孙节点 要用descendants 属性
+    for i, child in enumerate(soup.p.descendants):
+        print(i, child)
+    """
+    输出:
+    0 
+    
+    1 <a class="J_MemberPunch h" data-spm="d3" href="//taojinbi.taobao.com/index.htm?auto_take=true">
+    <span class="tbh-icon">
+    </span>
+             领淘金币抵钱
+            </a>
+    2 
+    
+    3 <span class="tbh-icon">
+    </span>
+    4 
+    
+    5 
+             领淘金币抵钱
+            
+    6 
+    
+    7 <a class="J_MemberClub h club" href="//vip.taobao.com">
+    <span class="tbh-icon">
+    </span>
+             会员俱乐部
+            </a>
+    8 
+    
+    9 <span class="tbh-icon">
+    </span>
+    10 
+    
+    11 
+             会员俱乐部
+            
+    12 
+
+    """
+    # 父节点 和 祖先节点 用parents 属性
+    print(soup.p.a)
+    print(list(enumerate(soup.p.a.parents)))
+    # 兄弟节点选择 用next_sibling 和 previ_sibling 分别获取下一个和上一个兄弟元素 next_siblings 和 previ_sibings 获取前面的 和后面的兄弟元素
